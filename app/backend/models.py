0c9ae00ed47e52de964ea4d910844f0841b979e0
d1g1t4t0r <ya.claude2015@ya.ru>
2021-04-24 22:58:45 +0300

    New entities for DB

--- app/backend/models.py
+++ app/backend/models.py
@@ -3,6 +3,23 @@ from back import db
 class Artist(db.Model):
     __tablename__ = 'artist'
     artist_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    info = db.Column(db.String(1000), unique=True, nullable=False)
-    photo = db.Column(db.String(100), unique=True, nullable=False) #base64 string
+    artist_name = db.Column(db.String(100), unique=False, nullable=False)
+    artist_info = db.Column(db.String(1000), unique=False, nullable=False)
+    artist_photo = db.Column(db.String(100), unique=False, nullable=False) #base64 string
+
+class Concert(db.Model):
+    __tablename__ = 'concert'
+    concert_id = db.Column(db.Integer, primary_key=True)
+    concert_name = db.Column(db.String(100), unique=False, nullable=False)
+    concert_info = db.Column(db.String(1000), unique=False, nullable=False)
+    concert_photo = db.Column(db.String(100), unique=False, nullable=False) #base64 string
+
+class User(db.Model):
+    __tablename__ = 'user'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    user_password = db.Column(db.String(100), unique=False, nullable=False)
+    user_role = db.Column(db.Integer, unique=False, nullable=False)
+    user_photo = db.Column(db.String(100), unique=False, nullable=False) #base64 string
+    user_spotify_token = db.Column(db.String(100), unique=False, nullable=True)
+    user_gcalendar_token = db.Column(db.String(100), unique=False, nullable=True)
