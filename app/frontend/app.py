37f25172dbfc4df2044964a9c863941674cbeb83
kathiir <49533622+kathiir@users.noreply.github.com>
2021-04-25 22:13:08 +0300

    prototype menu

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import os
+from flask_babel import Babel
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+app.config['LANGUAGES'] = {
+    'en': 'English',
+    'ru': 'Russian'
+}
+
+bootstrap = Bootstrap(app)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index_page():
+    return render_template(
+        'base.html'
+    )
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    print(' http://127.0.0.1:5000/')
     app.run(host='0.0.0.0', port=port)
+    # app.run()
